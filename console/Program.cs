1e201de5b3fddcfe28e58b8921514ed205e3f45f
rogeriolippert <rogeriolippert35@gmail.com>
2017-10-21 16:05:44 -0200

    criação do projeto console

---
+++ console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using lib;
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            Calculadora calc = new Calculadora();
+            int r = calc.Soma(2,3);
+            Console.WriteLine(r);
+        }
+    }
+}
