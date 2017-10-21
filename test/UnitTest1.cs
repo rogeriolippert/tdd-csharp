3112c5ff2191a0d233b1ca88b43eb2baf18135d1
rogeriolippert <rogeriolippert35@gmail.com>
2017-10-21 14:52:03 -0200

    Criado projeto de teste com teste de soma

---
+++ test/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new Calculadora();
+            int r = calc.Soma(2,3);
+            Assert.Equal(5, r);
+        }
+    }
+}
